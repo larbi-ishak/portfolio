4a2e7beaa416a5886401164fa3eccf796bdd95dd
LARBI Ishak <ishak.larbi@inttic.dz>
2023-08-13 21:44:36 +0000

    some personal data

--- app/page.tsx
+++ app/page.tsx
@@ -29,20 +29,20 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+				LARBI ISHAK
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
+					Hi, my name is LARBI ISHAK, I'm building serverless and open source
 					solutions at{" "}
 					<Link
 						target="_blank"
 						href="https://upstash.com"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						Upstash
+						AMB Internationl
 					</Link>
 
 					<br />
@@ -52,7 +52,7 @@ export default function Home() {
 						href="https://unkey.dev"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						unkey.dev
+						open source and Freelance projects
 					</Link>{" "}
 					at night.
 				</h2>
