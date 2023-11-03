56eada0f199b4cc2654cd67be5c2569c8dc687a7
LARBI Ishak <ishak.larbi@inttic.dz>
2023-11-03 21:51:44 +0000

    adding  skills

--- app/page.tsx
+++ app/page.tsx
@@ -91,8 +91,19 @@ export default function Home() {
           </Container>
         </div>
       </section>
-      <section>
-        skills
+      <section >
+        <h4 className="underline p-4 text-lg">Skills</h4>
+        <ul>
+          <li>Frontend: HTML, CSS, JS</li>
+          <li>Backend: Nodejs, Go, Python</li>
+          <li>Databases: SQL(PostgreSQL), NoSql(Mongo, Redis)</li>
+          <li>Programming Languages: Js/Typescript, Python, Go, C++</li>
+          <li>Frameworks: React&Nextjs, Vue&Nuxt, express, Django, Gin/Fiber</li>
+          <li>Technologies: WebSockets, GraphQL, WebRTC </li>
+          <li>Tools: Linux(RedHat), Git, Docker, Jira </li>
+          <li>Cloud: GCP, Huawei Cloud</li>
+          <li>Networks: CCNA</li>
+        </ul>
       </section>
     </main>
   );
