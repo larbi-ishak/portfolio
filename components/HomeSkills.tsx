fd0867d7c2f6b555946ab215f5b5560ae22bd494
larbi-ishak <ishak.larbi@inttic.dz>
2024-01-12 23:36:09 +0100

    fixing home skills

--- components/HomeSkills.tsx
+++ components/HomeSkills.tsx
@@ -1,20 +1,20 @@
-const HomeSkills = ()=>{
-    return (
-      <section >
-        <h4 className="underline p-4 text-lg">Skills</h4>
-        <ul>
-          <li>Frontend: HTML, CSS, JS/TS</li>
-          <li>Backend: Nodejs, Go, Python</li>
-          <li>Databases: SQL(PostgreSQL), NoSql(Mongo, Redis)</li>
-          <li>Frameworks: React&Nextjs, Vue&Nuxt, express, Django, Gin/Fiber</li>
-          <li>Testing: Jest, Vitest</li>
-          <li>Tools: Linux(RedHat), Git, Docker, Jira</li>
-          <li>Cloud: GCP, Huawei Cloud</li>
-          <li>Networks: CCNA</li>
-          <li>Communications and collaboration, Problem-sovling, Critical thinking</li>
-        </ul>
-     </section>
-    )
+const HomeSkills = () => {
+  return (
+    <section >
+      <h4 className="underline p-4 text-lg">Skills</h4>
+      <ul>
+        <li>Frontend: HTML, CSS, JS/TS</li>
+        <li>Backend: Nodejs, Go, Python</li>
+        <li>Databases: SQL(PostgreSQL), NoSql(Mongo, Redis)</li>
+        <li>Frameworks: React&Nextjs, Vue&Nuxt, Nestjs(fasitfy), Django, Gin</li>
+        <li>Testing: Playwright, Vitest</li>
+        <li>Tools: Linux, Git, Docker</li>
+        <li>Cloud: GCP, Huawei Cloud</li>
+        <li>Network: CCNA</li>
+        <li>Communications and collaboration, Problem-sovling, Critical thinking</li>
+      </ul>
+    </section>
+  )
 
 }
 
